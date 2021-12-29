3635958ea119d279bf6c8e526e50adcdc42e83e0
Ward Alhadid <wf.hadid94@gmail.com>
2021-12-28 23:47:03 -0500

    format date & amount

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -10,7 +10,7 @@ return (
         <h3>{description}</h3>
     </Link>
         <p>
-          {numeral(amount).format('$0,0.00')}
+          {numeral(amount / 100).format('$0,0.00')}
            - 
            {moment(createdAt).format('MMMM Do, YYYY')}
            </p>
